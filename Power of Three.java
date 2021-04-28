7f8ef7ef065c565b50b4f719046fe0548edfc8c9
Mike Tran <ngocmike238@gmail.com>
2021-04-28 01:11:57 -0400

    Create Power of Three.java

---
+++ Power of Three.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfThree(int n) 
+    {
+        double a = Math.log(n) / Math.log(3);
+        return Math.abs(a - Math.rint(a)) <= 0.00000000000001;
+    }
+}
